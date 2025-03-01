97e6d11d347522990084509dc10dd1952fdc2e8e
linroot <nevalions@live.com>
2025-03-01 11:32:18 +0300

    treesitter refactor

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,39 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+      auto_install = true,
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+      build = ":TSUpdate",
+    },
+  },
+}
