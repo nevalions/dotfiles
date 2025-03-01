c21c0e7a5e2bad7920d8d28613d88c5d64d595af
linroot <nevalions@live.com>
2025-03-01 13:12:27 +0300

    comments refactor using defualt lazyvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
--- vim.keymap.del("n", "<leader>", "g-c")
 
 vim.api.nvim_set_keymap("n", "<C-z>", "u", { noremap = true, silent = true })
 
