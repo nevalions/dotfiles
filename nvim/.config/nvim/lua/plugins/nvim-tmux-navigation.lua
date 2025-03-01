079cda796b42af34c6133c4b5eb7de56abc5138c
linroot <nevalions@live.com>
2025-03-01 11:14:03 +0300

    nvim ctr+hjkl binds

--- nvim/.config/nvim/lua/plugins/nvim-tmux-navigation.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -1,10 +1,27 @@
 return {
   "alexghergh/nvim-tmux-navigation",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<CR>", desc = "Navigate left" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<CR>", desc = "Navigate down" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<CR>", desc = "Navigate up" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<CR>", desc = "Navigate right" },
+  },
   config = function()
-    require("nvim-tmux-navigation").setup({})
-    vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", {})
-    vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
-    vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
-    vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
+    require("nvim-tmux-navigation").setup({
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+      },
+    })
   end,
 }
