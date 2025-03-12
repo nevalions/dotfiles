a81fb21f43cf831c319546f8dee199e46411eab7
linroot <nevalions@gmail.com>
2025-03-12 16:56:16 +0300

    faster loading yanky keymaps

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -105,3 +105,24 @@ map('v', '<C-e>', '$', { noremap = true })
 
 -- No Neck Pain
 map('n', '<leader>nn', ':NoNeckPain<CR>', { desc = 'NoNeckPain' })
+
+-- Yanky
+vim.keymap.set({ 'n', 'x' }, 'p', '<Plug>(YankyPutAfter)')
+vim.keymap.set({ 'n', 'x' }, 'P', '<Plug>(YankyPutBefore)')
+vim.keymap.set({ 'n', 'x' }, 'gp', '<Plug>(YankyGPutAfter)')
+vim.keymap.set({ 'n', 'x' }, 'gP', '<Plug>(YankyGPutBefore)')
+
+vim.keymap.set('n', '<c-p>', '<Plug>(YankyPreviousEntry)')
+vim.keymap.set('n', '<c-n>', '<Plug>(YankyNextEntry)')
+vim.keymap.set('n', ']p', '<Plug>(YankyPutIndentAfterLinewise)')
+vim.keymap.set('n', '[p', '<Plug>(YankyPutIndentBeforeLinewise)')
+vim.keymap.set('n', ']P', '<Plug>(YankyPutIndentAfterLinewise)')
+vim.keymap.set('n', '[P', '<Plug>(YankyPutIndentBeforeLinewise)')
+
+vim.keymap.set('n', '>p', '<Plug>(YankyPutIndentAfterShiftRight)')
+vim.keymap.set('n', '<p', '<Plug>(YankyPutIndentAfterShiftLeft)')
+vim.keymap.set('n', '>P', '<Plug>(YankyPutIndentBeforeShiftRight)')
+vim.keymap.set('n', '<P', '<Plug>(YankyPutIndentBeforeShiftLeft)')
+
+vim.keymap.set('n', '=p', '<Plug>(YankyPutAfterFilter)')
+vim.keymap.set('n', '=P', '<Plug>(YankyPutBeforeFilter)')
